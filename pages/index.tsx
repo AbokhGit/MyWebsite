d5dd09d2bf59834d156849c3783e293ca0447a5c
Abokh <just.abokh@gmail.com>
2023-08-20 19:28:19 +0500

    final

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import MovieList from '@/components/MovieList';
 import useMovieList from '@/hooks/useMovieList';
 import useFavorites from '@/hooks/useFavorites';
 import InfoModal from '@/components/InfoModal';
-import useInfoModal from '@/hooks/useInfoModal'
+import useInfoModal from '@/hooks/useInfoModal';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -32,32 +32,15 @@ export default function Home() {
 
   return (
     <>
-<<<<<<< HEAD
       <Navbar/>
-      <div className='lg:px-56 px-20'>
+      <div className='lg:px-56 px-2'>
         <InfoModal visible={isOpen} onClose={closeModal}/>
         <BillBoard/>
         <div className='pb-40'>
           <MovieList title='Tranding Now' data={movies}/>
           <MovieList title='My List' data={favorites}/>
         </div>
-        
-=======
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2615067240084342"
-     crossOrigin="anonymous"></script>
-      <InfoModal visible={isOpen} onClose={closeModal}/>
-      <Navbar/>
-      <BillBoard/>
-      <div className='pb-40'>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2615067240084342"
-     crossOrigin="anonymous"></script>
-        <MovieList title='Tranding Now' data={movies}/>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2615067240084342"
-     crossOrigin="anonymous"></script>
-        <MovieList title='My List' data={favorites}/>
->>>>>>> 0bcc47b4c97eb7eac386489037b149931f26a753
       </div>
-      
     </>
   )
 }
